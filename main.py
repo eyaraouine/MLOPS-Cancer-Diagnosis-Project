be5edf679e5c85f30932f6b089d53235ddf16067
eyaraouine <eyaraouine15@gmail.com>
2024-08-03 20:31:54 +0100

    added CNN base model preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.data_ingestion_stage_01 import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.prepare_base_cnn_model_stage_02 import PrepareBaseModelTrainingPipeline
 
 
 
@@ -14,3 +15,16 @@ try:
 except Exception as e:
     logger.exception(f"Exception occurred during {STAGE_NAME}: {str(e)}")
     raise e
+
+
+
+STAGE_NAME = "Prepare base CNN model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
